5347ffda224f52e3bf44055edce37384fefe54e0
Hasan <hmust92@gmail.com>
2020-12-12 07:25:12 -0600

    adds the correct ui for errors and loading

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import useSWR from "swr";
 import axios from "axios";
+import ErrorPage from "next/error";
+import Image from "next/image";
 import MatchDayCard from "../components/MatchDayCard";
 
 const fetcher = (url) => axios.get(url).then((response) => response.data);
 
 const Index = () => {
     const { data: events, error } = useSWR("/api/matchday", fetcher);
-    if (error) return <div>Failed to load</div>;
-    if (!events) return <div>Loading...</div>;
+
+    if (error) return <ErrorPage statusCode={error.response.status} />;
+
+    if (!events)
+        return (
+            <div className="loading">
+                <Image src="/loading-indicator.svg" width={60} height={60} />
+                <style jsx>{`
+                    .loading {
+                        display: flex;
+                        height: 100vh;
+                        justify-content: center;
+                    }
+                `}</style>
+            </div>
+        );
 
     return (
         <div className="p24">
