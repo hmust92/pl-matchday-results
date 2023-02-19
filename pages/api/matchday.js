fe32edc957041209865fcd967d1ea64d57b877c7
Hasan Mustafa <35225030+hmust92@users.noreply.github.com>
2023-02-19 01:59:11 -0600

    Update matchday.js

--- pages/api/matchday.js
+++ pages/api/matchday.js
@@ -1,34 +1,34 @@
 import axios from "axios";
 
-const matchDay = Number(process.env.MATCH_DAY);
+const matchDay = process.env.MATCH_DAY;
 
 export default function matchDayHandler(req, res) {
-    axios
-        .get(
-            "https://prod-public-api.livescore.com/v1/api/react/stage/soccer/england/premier-league/-6.00"
-        )
-        .then((results) => {
-            if (
-                !results.data.Stages ||
-                !results.data.Stages[0] ||
-                !results.data.Stages[0].Events
-            ) {
-                res.status(404);
-                return;
-            }
+  axios
+    .get(
+      "https://prod-public-api.livescore.com/v1/api/react/stage/soccer/england/premier-league/-6.00"
+    )
+    .then((results) => {
+      if (
+        !results.data.Stages ||
+        !results.data.Stages[0] ||
+        !results.data.Stages[0].Events
+      ) {
+        res.status(404);
+        return;
+      }
 
-            res.status(200).json({
-                matchDay: matchDay,
-                events: results.data.Stages[0].Events.filter(
-                    (event) => event.Ern === matchDay
-                ).map((event) => ({
-                    id: event.Eid,
-                    homeTeamName: event.T1[0].Nm,
-                    awayTeamName: event.T2[0].Nm,
-                    status: event.Eps,
-                    homeTeamScore: Number(event.Tr1),
-                    awayTeamScore: Number(event.Tr2),
-                })),
-            });
-        });
+      res.status(200).json({
+        matchDay: matchDay,
+        events: results.data.Stages[0].Events.filter(
+          (event) => event.ErnInf === matchDay
+        ).map((event) => ({
+          id: event.Eid,
+          homeTeamName: event.T1[0].Nm,
+          awayTeamName: event.T2[0].Nm,
+          status: event.Eps,
+          homeTeamScore: Number(event.Tr1),
+          awayTeamScore: Number(event.Tr2),
+        })),
+      });
+    });
 }
